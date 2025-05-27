737b62fa7ec0ff222e607c7bc35ce5154da8a61b
SothisKrieg <ftatilon@gmail.com>
2025-05-27 16:58:24 +0200

    Change duration format to always be HH:MM:SS

--- app/utils.ts
+++ app/utils.ts
@@ -28,10 +28,7 @@ export function formatDuration(seconds: number): string {
     const minutes = Math.floor((seconds % 3600) / 60);
     const remainingSeconds = Math.floor(seconds % 60);
 
-    if (hours > 0) {
-        return `${hours}:${minutes
-            .toString()
-            .padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
-    }
-    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+    return `${hours.toString().padStart(2, '0')}:${minutes
+        .toString()
+        .padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
 }
