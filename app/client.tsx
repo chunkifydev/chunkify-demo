e3f075fa578dc49ecce649df0a6aa7876d2f371e
SothisKrieg <ftatilon@gmail.com>
2025-06-24 08:54:01 +0200

    Updated client init to be in lie with the updated TS library

--- app/client.tsx
+++ app/client.tsx
@@ -1,6 +1,6 @@
 import { createClient } from 'chunkify';
 
-export const client = createClient(
-    process.env.CHUNKIFY_TEAM_TOKEN || '',
-    process.env.CHUNKIFY_PROJECT_TOKEN || ''
-);
+export const client = createClient({
+    teamToken: process.env.CHUNKIFY_TEAM_TOKEN || '',
+    projectToken: process.env.CHUNKIFY_PROJECT_TOKEN || '',
+});
