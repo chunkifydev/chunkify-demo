6ea1ee67070006a4c8c08372e8b3c3e001963682
SothisKrieg <ftatilon@gmail.com>
2025-08-27 16:55:14 +0200

    Ignore Hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,10 +26,10 @@ export default function RootLayout({
     return (
         <html
             lang="en"
-            suppressHydrationWarning
         >
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                suppressHydrationWarning
             >
                 <ThemeProvider
                     attribute="class"
